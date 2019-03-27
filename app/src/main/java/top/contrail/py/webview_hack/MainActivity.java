eb44f8bf39060be63f7ace1add751f802c4f77e9
Py Zhao <zhaopy4721@outlook.com>
2019-03-27 09:56:55 +0800

    add webview

--- app/src/main/java/top/contrail/py/webview_hack/MainActivity.java
+++ app/src/main/java/top/contrail/py/webview_hack/MainActivity.java
@@ -1,13 +1,40 @@
 package top.contrail.py.webview_hack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button url_go=null;
+    EditText url_edit=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        url_go = (Button) findViewById(R.id.urlGo);
+        url_edit=(EditText) findViewById(R.id.editText);
+
+        url_go.setOnClickListener(new View.OnClickListener() {
+
+            @Override public void onClick(View v) {
+
+                String url = url_edit.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, WebActivity.class);
+                intent.putExtra("url",url);
+                startActivity(intent);
+
+            }
+
+        });
+
+
     }
 }
---
+++ app/src/main/java/top/contrail/py/webview_hack/WebActivity.java
@@ -0,0 +1,68 @@
+package top.contrail.py.webview_hack;
+
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebActivity extends AppCompatActivity {
+
+    WebView webView = null;
+    private static final String TAG = "WebActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+
+        webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+        webSettings.setAllowFileAccess(true); //设置可以访问文件
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+
+        String url = getIntent().getStringExtra("url");
+        Log.d(TAG, url);
+
+        webView.loadUrl(url);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void  onPageStarted(WebView view, String url, Bitmap favicon) {
+                Log.d(TAG, "The page now start.");
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.d(TAG, "The page now Finished.");
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();    //表示等待证书响应
+                // handler.cancel();      //表示挂起连接，为默认方式
+                // handler.handleMessage(null);    //可做其他处理
+            }
+
+        });
+
+    }
+}
